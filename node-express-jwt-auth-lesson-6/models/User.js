9c94694595b2637bde6b220ecd2c660c03f3db18
Douglas Mogoba <mogobanyamwaro@gmail.com>
2021-05-04 06:10:48 +0300

    all the lessions

---
+++ node-express-jwt-auth-lesson-6/models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const { isEmail } = require('validator');
+const bycrypt = require('bcrypt')
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: [true, 'Please enter an email'],
+    unique: true,
+    lowercase: true,
+    validate: [isEmail, 'Please enter a valid email']
+  },
+  password: {
+    type: String,
+    required: [true, 'Please enter a password'],
+    minlength: [6, 'Minimum password length is 6 characters'],
+  }
+});
+
+userSchema.pre('save',async function(next){
+  const salt = await bycrypt.genSalt()
+  this.password = await bycrypt.hash(this.password, salt)
+  next()
+})
+const User = mongoose.model('user', userSchema);
+
+module.exports = User;
